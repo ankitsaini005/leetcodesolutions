d4971d382046bcec9c10d3b3d663044c3f216ec7
ankitsaini005 <91313971+ankitsaini005@users.noreply.github.com>
2023-07-16 18:12:41 +0530

    Time: 2190 ms (24.14%), Space: 154.3 MB (55.17%) - LeetHub

--- 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.cpp
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.cpp
@@ -1,40 +1,55 @@
 class Solution {
 public:
-    int ans=0;
-    void check(string &word,vector<int>&mp1)
+    int check(string &word,vector<int>&mp1,int mid)
     {
         int n=word.length();
         int st=0;
         int en=0;
-        
-    
-        while((en<n)&&(st<n))
+        // string temp;
+        // for(int i=st;i<=en;i++)
+        // {
+        //     temp+=word[i];
+        // }
+         int len=0;
+        // if((en<n)&&mp1[en].size()>0)
+        // {
+        //     int len=*(mp1[en].begin());
+        //     if(len<=(en-st+1))
+        //     st=en-(len-1)+1;
+        // }
+        // if(en<n)
+        // len=max(len,en-st+1);
+        //cout<<st<<" "<<en<<"\n";
+        while((en<n)&&(st<n)&&(st<=en))
         {
            
                // en++;
             int f=0;
-               if(mp1[en]!=0)
+               if((en<n)&&mp1[en]!=0)
                {
                  int len1=mp1[en];
                    if(len1<=(en-st+1))
-                   {st=en-(len1-1)+1;
-                   // cout<<st<<" "<<en<<"\n";
-                     ans=max(ans,en-st+1);
-                   }
-                 else 
+                 st=en-(len1-1)+1;
+                 if(en<st)
                  {
-                     //st=en+1;
-                     ans=max(ans,en-st+1);
+                     st=en+1;
+                     en=st;
+                     continue;
                  }
                }
-              else
-                ans=max(ans,en-st+1);
+              
+            
+          // 
+                //if(en<n)
+                len=max(len,en-st+1);
             
                 en++;
             
         }
-      // ans=len;
-       // return 0;
+        if(len>=mid)
+            return 1;
+        else
+            return 0;
     }
     int longestValidSubstring(string word, vector<string>& forbidden) 
     {
@@ -46,7 +61,11 @@ public:
         }
         int n=word.length();
         int m=forbidden.size();
-      
+        
+        // for(int i=0;i<forbidden.size();i++)
+        // {
+        //     reverse(forbidden[i].begin(),forbidden[i].end());
+        // }
         for(int i=0;i<m;i++)
         {
             mp[forbidden[i]]++;
@@ -55,7 +74,7 @@ public:
         {
             string temp;
             temp+=word[i];
-            if(mp[temp]>0)
+            if((mp.find(temp)!=mp.end())&&mp[temp]>0)
             {
                 mp1[i]=1;
             }
@@ -67,21 +86,30 @@ public:
           {
               temp+=word[j];
             //  cout<<temp<<" ";
-              if(mp.find(temp)!=mp.end()&&mp[temp]>0)
+              if((mp.find(temp)!=mp.end())&&mp[temp]>0)
               {
-                 
-                      mp1[j]=(j-i+1);
-                  
-                  
+                  mp1[j]=j-i+1;
               }
           }
         }
-        
+        // for(int i=0;i<n;i++)
+        // {
+        //     cout<<i<<" "<<mp1[i]<<"\n";
+        // }
         int l=0;
         int r=word.length();
-        
-        check(word,mp1);
-      
+        int ans=0;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(check(word,mp1,mid))
+            {
+                ans=max(ans,mid);
+                l=mid+1;
+            }
+            else
+                r=mid-1;
+        }
         return ans;
     }
 };
